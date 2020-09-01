39a21f22ad7a2dbe50c51d6fe316cddeaf586fe0
0x1306a94 <0x1306a94@gmail.com>
2020-09-01 16:14:11 +0800

    添加EBO使用

--- OpenGL-GLFW/OpenGL-GLFW/main.cpp
+++ OpenGL-GLFW/OpenGL-GLFW/main.cpp
@@ -20,7 +20,7 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
 	//如果按下ESC，把windowShouldClose设置为True，外面的循环会关闭应用
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
-	std::cout << "ESC" << mode;
+	std::cout << "ESC " << mode << std::endl;
 }
 
 static void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
@@ -94,8 +94,10 @@ int main(int argc, const char *argv[]) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // for Mac OS X
+//    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
 	std::cout << "glfw version: " << glfwGetVersionString() << std::endl;
 	//创建窗口以及上下文
@@ -122,15 +124,19 @@ int main(int argc, const char *argv[]) {
 
 	/* clang-format off */
     GLfloat vertices[] = {
-        0.5f, 0.5f, 0.0f,   // 右上角
-		0.5f, -0.5f, 0.0f,  // 右下角
-		-0.5f, -0.5f, 0.0f, // 左下角
-		-0.5f, 0.5f, 0.0f   // 左上角
+		// 右上角		  // 颜色
+		0.5f, 0.5f, 0.0f, 1.0, 0.0, 0.0,
+		// 右下角
+		0.5f, -0.5f, 0.0f, 0.0, 1.0, 0.0,
+		// 左下角
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+		// 左上角
+		-0.5f, 0.5f, 0.0f, 0.3, 0.5, 0.5,
     };
 
 	GLuint indices[] = { // 注意索引从0开始!
-		0, 1, 3, // 第一个三角形
-		1, 2, 3  // 第二个三角形
+		0, 2, 6, // 第一个三角形
+		2, 4, 6  // 第二个三角形
 	};
 	/* clang-format on */
 
@@ -141,29 +147,23 @@ int main(int argc, const char *argv[]) {
 	GLuint shaderProgram           = CreateShader(vertexShader, fragmentShader);
 	glUseProgram(shaderProgram);
 
-	GLuint VBO;
+	GLuint VBO, VAO, EBO;
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
 	glEnableVertexAttribArray(0);
 
-	GLuint VAO;
-	glGenVertexArrays(1, &VAO);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
 
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	GLuint EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
-	glEnableVertexAttribArray(0);
-	glBindVertexArray(0);
 
 	while (!glfwWindowShouldClose(window)) {
 
@@ -174,20 +174,21 @@ int main(int argc, const char *argv[]) {
 		glUseProgram(shaderProgram);
 
 		// 更新uniform颜色
-		float timeValue           = glfwGetTime();
-		float redValue            = (sin(timeValue) / 2.0f) + 0.2f;
-		float greenValue          = (sin(timeValue) / 2.0f) + 0.5f;
-		float blueValue           = (sin(timeValue) / 2.0f) + 0.4f;
-		GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0);
+		//		float timeValue           = glfwGetTime();
+		//		float redValue            = (sin(timeValue) / 2.0f) + 0.2f;
+		//		float greenValue          = (sin(timeValue) / 2.0f) + 0.5f;
+		//		float blueValue           = (sin(timeValue) / 2.0f) + 0.4f;
+		//		GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		//		glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0);
 
 		// 绘制三角形
-//		glBindVertexArray(VAO);
-//		glDrawArrays(GL_TRIANGLES, 0, 3);
-//		glBindVertexArray(0);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 
 		// 绘制矩形
 		glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
@@ -195,6 +196,9 @@ int main(int argc, const char *argv[]) {
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteShader(shaderProgram);
 	glfwTerminate();
 	return EXIT_SUCCESS;
--- OpenGL-GLFW/Shader/Shader.fsh
+++ OpenGL-GLFW/Shader/Shader.fsh
@@ -1,9 +1,9 @@
 #version 410 core
 
-layout (location = 0) out vec4 color;
+in vec3 ourColor;
 
-uniform vec4 ourColor; // 在OpenGL程序代码中设定这个变量
+out vec4 FragColor;
 
 void main() {
-	color = ourColor;//vec4(1.0, 0.5, 0, 1.0);
+	FragColor = vec4(ourColor, 1.0);
 }
